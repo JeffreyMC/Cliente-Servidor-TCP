f65639a9be2653cb95c80b290a9a3a44d31e1735
Jeffrey <jmunozcastro16@gmail.com>
2019-03-26 23:49:58 -0600

    Prueba de Cliente-Servidor TCP

---
+++ Cliente/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient cliente = new TcpClient();
+            Console.WriteLine("Conectando al servidor...");
+            cliente.Connect("127.0.0.1", 50000);
+            Console.WriteLine("Cliente conectado");
+
+
+            //Escribir el mensaje que se quiere enviar
+            Console.Write("Escriba el mensaje a enviar: ");
+            string mensaje = Console.ReadLine();
+
+            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            Stream st = cliente.GetStream();
+
+            st.Write(buffer, 0, buffer.Length);
+
+            st.Flush();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace Servidor
+{
+    class Program
+    {
+        private static TcpListener servidor;
+        private static Thread escucha;
+        private static TcpClient client;
+        static void Main(string[] args)
+        {
+            servidor = new TcpListener(IPAddress.Parse("127.0.0.1"), 50000);
+            escucha = new Thread(new ThreadStart(clientes));
+            escucha.Start();
+        }
+
+        public static void clientes()
+        {
+            Console.WriteLine("Conectando servidor...");
+            servidor.Start();
+            Console.WriteLine("Servidor conectado. Esperando clientes...");
+            int contador = 1;
+            while (true)
+            {
+                client = servidor.AcceptTcpClient();
+                Console.WriteLine("Cliente {0} conectado", contador++);
+                Thread clienteThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clienteThread.Start(client);
+            }
+        }
+
+        public static void HandleClientComm(object client)
+        {
+            TcpClient tcpCliente = (TcpClient)client;
+            NetworkStream clientStream = tcpCliente.GetStream();
+            byte[] buffer = new byte[4096];
+
+            string responseData = String.Empty;
+
+            int bytes = clientStream.Read(buffer, 0, buffer.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(buffer,0, bytes);
+            Console.WriteLine("Texto recibido: {0}", responseData);
+            tcpCliente.Close();
+        }
+    }
+}
